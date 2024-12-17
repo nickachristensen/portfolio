adf8f6db44268a93ffea06b251321326e5a0804b
Nick Christensen <nickachristensen@gmail.com>
2024-12-17 12:54:32 -0500

    Adds sections and on scroll animations to laptop

--- src/app/components/Laptop.tsx
+++ src/app/components/Laptop.tsx
@@ -2,24 +2,103 @@ import * as THREE from "three"
 import { useGLTF } from "@react-three/drei"
 import gsap from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useRef, useEffect } from 'react'
 
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Laptop() {
     const topModel = useGLTF('/assets/Laptop_Top.glb')
     const bottomModel = useGLTF('/assets/Laptop_Bottom.glb')
+    const groupRef = useRef<THREE.Group>(null)
+    const topRef = useRef<THREE.Mesh>(null)
+    const bottomRef = useRef<THREE.Mesh>(null)
 
-    console.log(topModel.nodes)
+    useEffect(() => {
+        if (!groupRef.current || !topRef.current || !bottomRef.current) return;
+    
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: "#section1",
+              start: "top bottom",
+              end: "top top",
+              scrub: true,
+            },
+          })
+          .to(groupRef.current.rotation, {
+            x: 0.0,
+            ease: "power2.inOut",
+          })
+          .to(
+            groupRef.current.rotation,
+            {
+              y: Math.PI - 0.4,
+              ease: "power2.inOut",
+            },
+            ">"
+          )
+          .to(
+            topRef.current.rotation,
+            {
+              x: Math.PI / 2 + 0.1,
+              ease: "power2.inOut",
+            },
+            "<"
+          )
+          .to(
+            groupRef.current.position,
+            {
+              x: 1.3,
+              ease: "power2.inOut",
+            },
+            "<"
+          )
+          .to(
+            groupRef.current.scale,
+            {
+              x: 0.7,
+              y: 0.7,
+              z: 0.7,
+              ease: "power2.inOut",
+            },
+            "<"
+          );
+    
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: "#section2",
+              start: "top bottom",
+              end: "top top",
+              scrub: true,
+            },
+          })
+          .to(groupRef.current.rotation, {
+            y: Math.PI + 0.4,
+            ease: "power2.inOut",
+          })
+          .to(
+            groupRef.current.position,
+            {
+              x: -1.3,
+              ease: "power2.inOut",
+            },
+            "<"
+          );
+      });
+    
+
     return (
         <group
+        ref={groupRef}
         position={[0, -0.7, 0]} 
-        rotation={[0, Math.PI, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
         scale={1.3}
         >
-            <mesh>
+            <mesh ref={topRef}>
                 <primitive object={topModel.nodes.lapTop_Top} />
             </mesh>
-            <mesh>
+            <mesh ref={bottomRef}>
                 <primitive object={bottomModel.nodes.lapTop_Bottom} />
             </mesh>
         </group>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,47 @@ import Experience from "./components/Experience";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello
-      <Experience />
-    </div>
+    <main className="relative">
+        <section
+          id="hero"
+          className="h-screen flex items-end justify-center relative"
+        >
+          <div id="text-1" className="text-center mb-64 z-10">
+            <h1 className="text-8xl font-medium mb-5">Nick Christensen</h1>
+            <p className="text-4xl font-light text-gray-500">
+              Software Engineer // Designer
+            </p>
+          </div>
+        </section>
+        <section
+          id="section1"
+          className="h-screen flex items-center justify-start relative"
+        >
+          <div className="container mx-auto px-6">
+            <div id="text-2" className="max-w-lg">
+              <h2 className="text-7xl font-medium mb-4">Engineering</h2>
+              <p className="text-3xl font-light text-gray-500">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section
+          id="section2"
+          className="h-screen flex items-center justify-end relative"
+        >
+          <div id="text-3" className="container mx-auto px-6">
+            <div className="max-w-lg ml-auto text-right">
+              <h2 className="text-7xl font-medium mb-4 text-gray-800">
+                Design
+              </h2>
+              <p className="text-3xl font-light text-gray-500">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </section>
+        <Experience />
+      </main>
   );
 }
