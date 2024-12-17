70496458ef8650e24b44131ee31aae1a2df13064
Nick Christensen <nickachristensen@gmail.com>
2024-12-17 12:05:04 -0500

    Adds laptop model and component

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,25 +1,11 @@
 "use client"
 
 import * as THREE from "three"
-import { Box } from "@react-three/drei"
+import { Box, OrbitControls } from "@react-three/drei"
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useRef } from "react"
+import Laptop from "./Laptop"
 
-function TestBox() {
-    const boxRef = useRef<THREE.Mesh>(null)
-
-    useFrame(() => {
-        if (boxRef.current) {
-            boxRef.current.rotation.y += 0.01
-        }
-    })
-
-    return (
-        <Box ref={boxRef}>
-            <meshStandardMaterial color={"red"} />
-        </Box>
-    )
-}
 
 export default function Experience() {
 
@@ -27,7 +13,8 @@ export default function Experience() {
     <div className="fixed inset-0">
         <Canvas camera= {{ position: [0, 0, 5], fov: 60 }}>
             <directionalLight intensity={2} position={[1, 2, 3]} />
-            <TestBox />
+            <Laptop />
+            <axesHelper scale={2} />
         </Canvas>
     </div>
   );
--- src/app/components/Laptop.tsx
+++ src/app/components/Laptop.tsx
@@ -1,5 +1,27 @@
 import * as THREE from "three"
+import { useGLTF } from "@react-three/drei"
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-export function Laptop() {
+gsap.registerPlugin(ScrollTrigger)
 
+export default function Laptop() {
+    const topModel = useGLTF('/assets/Laptop_Top.glb')
+    const bottomModel = useGLTF('/assets/Laptop_Bottom.glb')
+
+    console.log(topModel.nodes)
+    return (
+        <group
+        position={[0, -0.7, 0]} 
+        rotation={[0, Math.PI, 0]}
+        scale={1.3}
+        >
+            <mesh>
+                <primitive object={topModel.nodes.lapTop_Top} />
+            </mesh>
+            <mesh>
+                <primitive object={bottomModel.nodes.lapTop_Bottom} />
+            </mesh>
+        </group>
+    )
 }
